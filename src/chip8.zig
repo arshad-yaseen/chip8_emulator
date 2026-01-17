08ced0fb463e03a44b52acfac0d7490fe24a182f
arshad <arshadpyaseen@gmail.com>
2026-01-17 11:10:28 +0530

    display base

--- src/chip8.zig
+++ src/chip8.zig
@@ -1,14 +1,7 @@
 // my own implementation by reading https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 
 const std = @import("std");
-
-const c = @cImport({
-    @cInclude("fenster.h");
-});
-
-const display_width = 64;
-const display_height = 32;
-const scale = 10;
+const Display = @import("display.zig").Display;
 
 const memory_size = 4096;
 const stack_size = 16;
@@ -22,17 +15,23 @@ pub const Chip8 = struct {
     stack: [stack_size]u16,
     V: [register_size]u8,
 
-
     delay_timer: u8,
     sound_timer: u8,
+
+    display: Display,
 
     pub fn init() Chip8 {
         var chip8 = Chip8{
             .memory = [_]u8{0} ** memory_size,
+
             .PC = 0x200,
             .I = 0,
+
             .stack = [_]u16{0} ** stack_size,
             .V = [_]u8{0} ** register_size,
+
+            .display = Display.init(),
+
             .delay_timer = 0,
             .sound_timer = 0,
         };
@@ -42,11 +41,10 @@ pub const Chip8 = struct {
         return chip8;
     }
 
-    pub fn run(_: *Chip8) anyerror!void {
-    }
+    pub fn run(_: *Chip8) anyerror!void {}
 
     fn loadFonts(self: *Chip8) void {
-        const font_set = [80]u8 {
+        const font_set = [80]u8{
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
             0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -62,7 +60,7 @@ pub const Chip8 = struct {
             0xF0, 0x80, 0x80, 0x80, 0xF0, // C
             0xE0, 0x90, 0x90, 0x90, 0xE0, // D
             0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
         };
 
         for (font_set, 0..) |pixel, i| {
---
+++ src/display.zig
@@ -0,0 +1,58 @@
+const c = @cImport({
+    @cInclude("fenster.h");
+});
+
+const fenster = c.fenster;
+
+const display_width = 64;
+const display_height = 32;
+const scale = 10;
+
+const display_size = display_width * display_height;
+const display_size_scaled = (display_width * scale) * (display_height * scale);
+const packed_pixels_size = display_size / 8;
+
+pub const Display = struct {
+    // packed
+    pixels: [packed_pixels_size]u8,
+
+    pub fn init() Display {
+        return .{
+            .pixels = [_]u8{0} ** packed_pixels_size,
+        };
+    }
+
+    pub inline fn getPixel(self: *Display, x: u8, y: u8) u1 {
+        const index = pixelIndex(x, y);
+        const byte = self.pixels[byteIndex(index)];
+        const offset = bitOffset(index);
+        return @intCast((byte >> offset) & 1);
+    }
+
+    pub inline fn isPixelOn(self: *Display, x: u8, y: u8) bool {
+        return self.getPixel(x, y) != 0;
+    }
+
+    // toggle pixel
+    pub inline fn togglePixel(self: *Display, x: u8, y: u8) void {
+        const index = pixelIndex(x, y);
+        const byte_idx = byteIndex(index);
+        const offset = bitOffset(index);
+
+        self.pixels[byte_idx] ^= @as(u8, 1) << offset;
+    }
+
+    inline fn pixelIndex(x: u8, y: u8) u12 {
+        return @as(u12, y) * display_width + x;
+    }
+
+    // get byte index in packed array
+    inline fn byteIndex(index: u12) usize {
+        return index / 8;
+    }
+
+    // get bit position within byte
+    inline fn bitOffset(index: u12) u3 {
+        return @intCast(7 - index % 8);
+    }
+};
